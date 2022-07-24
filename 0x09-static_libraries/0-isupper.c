fb890d94a2e6c5b162198c54a9fdf6190b15c944
ifeowo <ifeowo99@gmail.com>
2022-07-23 17:33:54 -0700

    A library is not a luxury

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isupper -> this is a function to check upper case alphas
+* @c: an integer paramer
+* Return: something but success
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strlen -> function to get the length of a string
+* @s: string pointer to passed to this function
+* Return: returns length of the string
+*/
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+return (i);
+}
