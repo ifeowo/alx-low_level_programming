07341ef4aeea6d5ca15a8fa160d2a4f739c9aac4
ifeowo <ifeowo99@gmail.com>
2022-08-18 18:07:36 -0700

    correct

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- *  * _pow - calculates (base ^ power)
- *   * @base: base of the exponent
- *    * @power: power of the exponent
- *     *
- *      * Return: value of (base ^ power)
- *       */
+ * _pow - calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ * Return: value of (base ^ power)
+ */
 unsigned long int _pow(unsigned int base, unsigned int power)
 {
-		unsigned long int num;
-			unsigned int i;
-
-				num = 1;
-					for (i = 1; i <= power; i++)
-								num *= base;
-						return (num);
+unsigned long int num;
+unsigned int i;
+num = 1;
+for (i = 1; i <= power; i++)
+num *= base;
+return (num);
 }
-
 /**
  * print_binary - prints a number in binary notation
  * @n: number to print
